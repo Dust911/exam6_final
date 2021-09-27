64180e23db6d183e1f8f920ebe6505f8270f3f66
Dust911 <51755722+Dust911@users.noreply.github.com>
2021-09-27 11:41:22 +0600

    Delete src directory

--- src/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const Lol = styled.div`
-    width: 100%;
-    height: 100vh;
-    background: #000000;
-`
--- src/utils/stringFormatter.js
+++
@@ -1,6 +0,0 @@
-export const splitBigNumber = value => {
-    const splitingString = value.toString().split('').reverse();
-    return splitingString.map((currentValue, index) => {
-        return (index % 3 === 0) ? `${currentValue} ` : currentValue;
-    }).reverse().join('');
-};
